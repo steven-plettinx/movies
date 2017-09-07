43053b0fc6a505047d065c45eb38c5b7f9994921
Steven Plettinx <steven.plettinx@gmail.com>
2017-09-07 14:40:55 +0200

    Initialized project. First useless commit. Because I have to.

---
+++ src/edu/ap/movies/MovieResource.java
@@ -0,0 +1,16 @@
+package edu.ap.movies;
+
+import java.io.*;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.json.*;
+import javax.servlet.ServletContext;
+
+
+@Path("/movie")
+public class MovieResource {
+
+    Jedis jedis = new Jedis()
+
+}
